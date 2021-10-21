597bed4b91bcf7613d2346b7c39bfe9d23ee5b66
egoncalv <erickbarros06@hotmail.com>
2021-10-21 22:07:31 +0100

    norminette fixes

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,11 +1,24 @@
-int ft_isdigit(int c)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egoncalv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/21 21:30:07 by egoncalv          #+#    #+#             */
+/*   Updated: 2021/10/21 22:04:57 by egoncalv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <libft.h>
+
+int	ft_isdigit(int c)
 {
-    if (c >= '0' && c <= '9')
-        return (1);
-    return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
 }
-#include <stdio.h>
 /*
+#include <stdio.h>
 int main ()
 {
     unsigned char   a = '4';
@@ -14,4 +27,4 @@ int main ()
     printf("is_digit (4): %d\n", ft_isdigit(a));
     printf("is_digit (a): %d\n", ft_isdigit(b));
 }
-*/   
+*/
--- ft_strlen.c
+++ ft_strlen.c
@@ -1,12 +1,25 @@
-#include <stddef.h>
-size_t  ft_strlen(const char *s)
-{
-    int i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egoncalv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/21 21:35:40 by egoncalv          #+#    #+#             */
+/*   Updated: 2021/10/21 22:06:22 by egoncalv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <libft.h>
 
-    i = 0;
-    while (s[i] != 0)
-        i++;
-    return (i);
+size_t
+	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != 0)
+		i++;
+	return (i);
 }
 /*
 #include <stdio.h>
