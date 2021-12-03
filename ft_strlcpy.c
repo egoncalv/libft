337b626c8ffad355426d9e593b8c519218060dca
Erick Gonçalves <erickbarros06@hotmail.com>
2021-12-03 00:37:39 +0000

    ok

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: egoncalv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 02:35:03 by egoncalv          #+#    #+#             */
-/*   Updated: 2021/11/20 18:16:13 by egoncalv         ###   ########.fr       */
+/*   Updated: 2021/12/03 00:36:00 by egoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,14 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	i;
 
 	i = 0;
-	while (src[i] && i < (dstsize))
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	while (src[i] && i < (dstsize - 1))
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	dst[i] = 0;
-	while (src[i])
-		i++;
-	return (i);
+	if (i < dstsize)
+		dst[i] = 0;
+	return (ft_strlen(src));
 }
