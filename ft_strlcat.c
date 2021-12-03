5e5e9dd1f31aaf45b72c9630d89759f22488f7dd
Erick Gonçalves <erickbarros06@hotmail.com>
2021-12-03 01:11:49 +0000

    passed all tests

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: egoncalv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 02:36:10 by egoncalv          #+#    #+#             */
-/*   Updated: 2021/11/20 11:48:56 by egoncalv         ###   ########.fr       */
+/*   Updated: 2021/12/03 01:11:33 by egoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,22 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
-	int		j;
+	size_t	j;
+	size_t	dst_len;
 
 	i = 0;
 	j = 0;
-	while (dst[i] != 0)
+	if (dstsize < ft_strlen(dst))
+		return (ft_strlen(src) + dstsize);
+	while (dst[i])
 		i++;
-	while (src[j] && i < dstsize)
+	dst_len = i;
+	while (src[j] && i < (dstsize - 1) && dstsize > 0)
 	{
 		dst[i] = src[j];
 		i++;
 		j++;
 	}
 	dst[i] = 0;
-	return (i);
+	return (ft_strlen(src) + dst_len);
 }
