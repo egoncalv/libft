7e21a701f5313cbe6f8e478caa961c1bd139c143
egoncalv <erickbarros06@hotmail.com>
2021-12-06 14:23:39 +0000

    ok

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,11 +6,16 @@
 /*   By: egoncalv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 13:19:04 by egoncalv          #+#    #+#             */
-/*   Updated: 2021/12/04 13:19:44 by egoncalv         ###   ########.fr       */
+/*   Updated: 2021/12/06 14:12:54 by egoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h>"
+
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
+	if (lst && new)
+	{
 
+	}
 }
