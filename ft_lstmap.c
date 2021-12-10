5d513f73830e44e0098f483665205791cd701230
Erick Gonçalves <erickbarros06@hotmail.com>
2021-12-10 18:37:41 +0000

    lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: egoncalv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 19:02:25 by egoncalv          #+#    #+#             */
-/*   Updated: 2021/12/07 19:25:36 by egoncalv         ###   ########.fr       */
+/*   Updated: 2021/12/10 18:27:12 by egoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,17 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new;
 	t_list	*tmp;
 
-	*new = ft_lstnew(f(lst->content));
-	lst = lst->next;
+	new = 0;
 	while (lst)
 	{
-		tmp = lst->next;
-		ft_lstadd_front(new, f(lst));
-		lst = tmp;
+		tmp = ft_lstnew(f(lst->content));
+		if (!tmp)
+		{
+			ft_lstclear(&new, del);
+			return (0);
+		}
+		ft_lstadd_back(&new, tmp);
+		lst = lst->next;
 	}
-	if (!new)
-		return 0;
-	return (*new);
+	return (new);
 }
