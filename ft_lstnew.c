dbb4086a90a70053fa42e5c2006a148b5f3d89f3
Erick Gonçalves <erickbarros06@hotmail.com>
2021-12-17 12:09:20 +0000

    lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: egoncalv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 13:04:49 by egoncalv          #+#    #+#             */
-/*   Updated: 2021/12/04 13:12:23 by egoncalv         ###   ########.fr       */
+/*   Updated: 2021/12/17 12:09:03 by egoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ t_list	*ft_lstnew(void *content)
 
 	new = (t_list *)malloc(sizeof(t_list));
 	if (!new)
-		return (0);
+		return (new = NULL);
 	new->content = content;
 	new->next = NULL;
 	return (new);
