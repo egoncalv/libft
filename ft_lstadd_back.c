5e4c9c32ddf3fd00de2efce9ebb18b75cd08f5e8
Erick Gonçalves <erickbarros06@hotmail.com>
2021-12-22 14:18:42 +0000

    bonus

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: egoncalv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 17:39:36 by egoncalv          #+#    #+#             */
-/*   Updated: 2021/12/06 18:21:39 by egoncalv         ###   ########.fr       */
+/*   Updated: 2021/12/22 14:18:08 by egoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
-	if (!*lst)
+	if (lst)
 	{
-		*lst = new;
-		return ;
+		if (*lst == NULL)
+			*lst = new;
+		else
+		{
+			last = ft_lstlast(*(lst));
+			last->next = new;
+		}
 	}
-	last = ft_lstlast(*lst);
-	last->next = last;
 }
